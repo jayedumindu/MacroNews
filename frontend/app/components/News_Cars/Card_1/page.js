bf7fa53260796c4a7dbab61c551f122cd7256c8a
tharinduHansika <tharindunirma@gmail.com>
2024-04-03 21:22:51 +0530

    CSS changed

--- frontend/app/components/News_Cars/Card_1/page.js
+++ frontend/app/components/News_Cars/Card_1/page.js
@@ -5,14 +5,14 @@ import { faSearch, faUser, faCalendarAlt, faComment } from '@fortawesome/free-so
 export default function page({ title, category, status, description, image, date }) {
   return (
     <div id="small card" className="flex flex-col lg:flex-row  mb-4">
-      <div className="lg:basis-1/2 xl:basis-1/3 bg-slate-700 h-52 px-6">
+      <div className="lg:basis-1/2 xl:basis-1/3 bg-slate-700 h-52 px-6" style={{ backgroundImage: `url("${image}")`, backgroundSize: 'auto', backgroundPosition: 'center',backgroundRepeat: 'no-repeat', overflow: 'hidden' }}>
         <div className="bg-red-700 py-2 px-3 w-28 mt-28 mb-2">
           {/* News Box */}
           {category}
         </div>
       </div>
       <div className="lg:basis-1/2 xl:basis-2/3  bg-slate-300 h-52 pl-6">
-        <h1 className="mb-4 mt-4 text-black text-xl">
+        <h1 className="mb-4 mt-4 text-black text-xl font-semibold">
           {/* A Discount Toner Cartridge Is Better Than Ever. */}
           {title}
         </h1>
@@ -33,7 +33,7 @@ export default function page({ title, category, status, description, image, date
         </div>
 
         <div>
-          <h1 className="text-black text-sm">
+          <h1 className="text-black text-sm truncate-3-lines overflow-hidden h-10">
             {/* Lorem ipsum dolor sit amet, consecteturadip isicing elit, sed do eiusmod tempor incididunt. */}
             {description}
           </h1>
